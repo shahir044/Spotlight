a23714119b14e08e9f2724bd178dbc594e25878a
shahir044 <81510842+shahir044@users.noreply.github.com>
2023-04-24 23:39:15 +0600

    About Page

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer className='footer p-10 bg-gray-700 text-primary-content footer-center'>
             <div>
                 <FaHashtag className='mr-2 text-2xl'/>
-                <p>Copyright &copy; {footerYear} Biman IT Division</p>
+                <p>Copyright &copy; {footerYear} Shahir Rahman</p>
             </div>
         </footer>
     )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,14 @@
+import {Link} from "react-router-dom";
+
 function About() {
     return(
-        <div>
-            <h5 className='text-xl font-bold mb-4'>ABOUT PAGE of Github Finder: Developed By Shahir BGIT</h5>
+        <div className='container card rounded'>
+            <h1 className='text-3xl font-bold mb-4'>Spotlight By Shahir Rahman <Link to={`/user/shahir044`} className='text-base-content text-opacity-70 text-blue-800'>
+                shahir044
+            </Link>
+            </h1>
+            <h3 className='text-xl  mb-4'>You can search for github users here, visit their profiles and see the necessary info of that user.</h3>
+            <h5>version: 1.0.0</h5>
         </div>
     )
 }
